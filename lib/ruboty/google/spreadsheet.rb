3857ece8700e1b5807d578eb35ab6cb94e30789c
Naoto Kaneko <naoty.k@gmail.com>
2015-03-08 22:05:55 +0900

    Remove unused files

--- lib/ruboty/google/spreadsheet.rb
+++
@@ -1,9 +0,0 @@
-require "ruboty/google/spreadsheet/version"
-
-module Ruboty
-  module Google
-    module Spreadsheet
-      # Your code goes here...
-    end
-  end
-end
--- lib/ruboty/google/spreadsheet/version.rb
+++
@@ -1,7 +0,0 @@
-module Ruboty
-  module Google
-    module Spreadsheet
-      VERSION = "0.0.1"
-    end
-  end
-end
